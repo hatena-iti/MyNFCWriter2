cad24637cbcab268faba37fbef4f2410438e8101
ITI Jumpei Nakayama <nakayama@iti.co.jp>
2014-05-07 11:03:31 +0900

    Modifies MainActivity class.

--- src/jp/co/iti/mynfcwriter2/MainActivity.java
+++ src/jp/co/iti/mynfcwriter2/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.view.Menu;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -17,11 +18,12 @@ public class MainActivity extends Activity {
 	private static final String TARGET_URL_MAP = "geo:35.6703915,139.7758437";
 //	private static final String TARGET_URL_YOUTUBE = "http://www.youtube.com/watch?v=sGF6bOi1NfA";
 	private static final String TARGET_URL_YOUTUBE = "vnd.youtube://sGF6bOi1NfA";
-//	private static final String TARGET_URL_TWITTER = "https://twitter.com/";
-	private static final String TARGET_URL_TWITTER = "Twitter://";
 	private static final String TARGET_URL_LINE = "http://line.me/R/msg/text/?somemessage";
+//	private static final String TARGET_URL_TWITTER = "twitter://";
+//	private static final String TARGET_URL_TWITTER = "https://twitter.com/";
 
 	private NfcWriter nfcWriter = null;
+	private String targetUrl = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,10 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		this.nfcWriter = new NfcWriter(this);
+		this.targetUrl = TARGET_URL_WEB;
+
+		// Set up radio group control.
+		this.setRadioGroup();
 	}
 
 	@Override
@@ -53,7 +59,7 @@ public class MainActivity extends Activity {
 	protected void onNewIntent(Intent intent) {
 		super.onNewIntent(intent);
 
-		if (this.nfcWriter.write(this, intent, TARGET_URL_WEB)) {
+		if (this.nfcWriter.write(this, intent, this.targetUrl)) {
 			Toast.makeText(this, "Completed.", Toast.LENGTH_SHORT).show();
 		} else {
 			Toast.makeText(this, this.nfcWriter.getErrorMessage(), Toast.LENGTH_SHORT).show();
@@ -65,6 +71,7 @@ public class MainActivity extends Activity {
 		super.onDestroy();
 
 		this.nfcWriter = null;
+		this.targetUrl = null;
 	}
 
 	@Override
@@ -79,5 +86,48 @@ public class MainActivity extends Activity {
 		intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP
 						| Intent.FLAG_ACTIVITY_NEW_TASK);
 		return PendingIntent.getActivity(this, 0, intent, 0);
+	}
+
+	// Set up radio group control.
+	private void setRadioGroup() {
+		RadioGroup radioGroup = (RadioGroup)findViewById(R.id.radioGroupFunction);
+		radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				// Change the target url.
+				
+				switch (checkedId) {
+				case R.id.radioWeb:
+					MainActivity.this.targetUrl = TARGET_URL_WEB;
+					break;
+				case R.id.radioTel:
+					MainActivity.this.targetUrl = TARGET_URL_TEL;
+					break;
+				case R.id.radioSms:
+					MainActivity.this.targetUrl = TARGET_URL_SMS;
+					break;
+				case R.id.radioMail:
+					MainActivity.this.targetUrl = TARGET_URL_MAIL;
+					break;
+				case R.id.radioMap:
+					MainActivity.this.targetUrl = TARGET_URL_MAP;
+					break;
+				case R.id.radioYoutube:
+					MainActivity.this.targetUrl = TARGET_URL_YOUTUBE;
+					break;
+				case R.id.radioLine:
+					MainActivity.this.targetUrl = TARGET_URL_LINE;
+					break;
+//				case R.id.radioTwitter:
+//					MainActivity.this.targetUrl = TARGET_URL_TWITTER;
+//					break;
+				}
+
+				Toast.makeText(MainActivity.this, 
+							MainActivity.this.targetUrl, 
+							Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 }
